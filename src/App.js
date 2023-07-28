4de31fb5b7f894ea11ae6f4e1d6eab07395e9672
SadikHajar <sadik.hajar@etu.uae.ac.ma>
2023-07-28 15:50:53 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home";
-import {Person} from "@mui/icons-material";
+import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
 
 
 function App() {
-  return <Profile/>
+  return <Profile />
 }
 
 export default App;
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,26 +1,105 @@
-import React from 'react';
 import "./Rightbar.css";
-import Online from '../../components/online/Online';
-
 import { Users } from "../../dummyData";
-export default function Rightbar({profile}){
-    return(
-        <div className='rightbar'>
-            <div className="rightbarWrapper ">
-                <div className="birthdayContainer">
-                <img className="birthdayImg " src="assets/gift.png" alt="" />
-                <span className="birthdayText">
-                    <b>Youssef Banani </b>and <b>3 other friends</b> have a birthday today.
-                </span>
-            </div>
-            <img src="assets/ad.png" alt="" className="rightbarAd" />
-            <h4 className="rightbarTitle">Online Friends</h4>
-            <ul className="rightbarFriendList">
-                {Users.map(u=>(
-                    <Online  key={u.id} user={u}/>
-                ))}
-            </ul>
-            </div>
+import Online from "../online/Online";
+
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
         </div>
-    )
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Tanger</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Morocco</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Sadik Hajar</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Sadik Meriem</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Laila Laila</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Youssef Bnani</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Soukayna Idrissi</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">Sohaib Manah</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
 }
